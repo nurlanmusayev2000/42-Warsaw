0910ab05af83307a9c6899e761bcde948e798c9c
Nurlan Musayev <nmusayev@c1r12s4.42warsaw.pl>
2025-03-17 19:03:29 +0100

    updates

---
+++ GET_NEXT_LINE/get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmusayev <nmusayev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/09 13:06:16 by nmusayev          #+#    #+#             */
+/*   Updated: 2025/03/09 14:29:11 by nmusayev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 12345
+# endif
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*read_first_line(int fd, char *text);
+char	*get_line(char *text);
+char	*clean_first_line(char *text);
+void	*ft_calloc(unsigned int count, unsigned int size);
+
+#endif
